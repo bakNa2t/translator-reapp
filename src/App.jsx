bd9c65133d21978497fd57866451a026ce0c06c5
bakNa2t <ok2.lock@gmail.com>
2024-08-26 14:04:00 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center", fontSize: "3rem", fontWeight: "bold" }}>
+        ReApp Translator
+      </h1>
+    </div>
+  );
+}
+
+export default App;
