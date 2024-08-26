091822820e965a83ab5d7fa633980bd515760b82
bakNa2t <ok2.lock@gmail.com>
2024-08-26 15:58:20 +0300

    Updated App's styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import AppStartpage from "./components/AppStartpage";
+
 function App() {
   return (
-    <div>
-      <h1 style={{ textAlign: "center", fontSize: "3rem", fontWeight: "bold" }}>
-        ReApp Translator
-      </h1>
+    <div className="w-full h-screen bg-gradient-to-l from-[#7dd3fc] to-[#065f46] flex justify-center items-center">
+      <div className="w-[90%] max-w-lg h-[70vh] bg-[#57534e] rounded-xl shadow-2xl shadow-gray-800 flex flex-col">
+        <AppStartpage />
+      </div>
     </div>
   );
 }
