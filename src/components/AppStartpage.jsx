6aa8d4613e44c4697efcd97d43c739a48de83731
bakNa2t <ok2.lock@gmail.com>
2024-09-01 19:23:12 +0300

    Updated shadow hover in AppStartpage

--- src/components/AppStartpage.jsx
+++ src/components/AppStartpage.jsx
@@ -9,7 +9,7 @@ function AppStartpage({ onOpen }) {
 
   return (
     <div className="w-full h-full flex flex-col justify-center items-center p-6 sm:p-12">
-      <div className="w-full h-64 bg-gradient-to-r from-[#30cfd0] to-[#330867] rounded-t-full rounded-bl-full flex flex-col justify-center text-[#abecd6] pr-6">
+      <div className="w-full h-64 bg-gradient-to-r from-[#30cfd0] to-[#330867] rounded-t-full rounded-bl-full flex flex-col justify-center text-[#abecd6] pr-6 hover:shadow-lg hover:shadow-sky-400 transition duration-200">
         <span className="font-shojumaru text-5xl sm:text-6xl text-left pl-8">
           Hello
         </span>
