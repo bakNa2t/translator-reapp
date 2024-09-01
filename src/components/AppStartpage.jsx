e3c238482297e30c47e8c31dca8f5b5e52d3e4ad
bakNa2t <ok2.lock@gmail.com>
2024-09-01 19:36:21 +0300

    Added AppBanner to AppStartpage

--- src/components/AppStartpage.jsx
+++ src/components/AppStartpage.jsx
@@ -1,6 +1,7 @@
 import PropTypes from "prop-types";
 
 import ButtonStart from "./ButtonStart";
+import AppBanner from "./AppBanner";
 
 function AppStartpage({ onOpen }) {
   AppStartpage.propTypes = {
@@ -9,16 +10,7 @@ function AppStartpage({ onOpen }) {
 
   return (
     <div className="w-full h-full flex flex-col justify-center items-center p-6 sm:p-12">
-      <div className="w-full h-64 bg-gradient-to-r from-[#30cfd0] to-[#330867] rounded-t-full rounded-bl-full flex flex-col justify-center text-[#abecd6] pr-6 hover:shadow-lg hover:shadow-sky-400 transition duration-200">
-        <span className="font-shojumaru text-5xl sm:text-6xl text-left pl-8">
-          Hello
-        </span>
-        <span className="text-2xl sm:text-3xl text-center">Ciao</span>
-        <span className="font-notoSansJp text-3xl sm:text-4xl text-right">
-          こんにちは
-        </span>
-        <span className="text-2xl sm:text-3xl text-right">Hola</span>
-      </div>
+      <AppBanner />
       <div className="w-full text-right space-y-5 my-20">
         <h1 className="font-righteous text-3xl sm:text-4xl text-sky-200 uppercase">
           <span className="text-2xl sm:text-2xl opacity-50 block text-sky-500">
