2683c7ffca636f1d04a5a208036ad17b5426d0ad
bakNa2t <ok2.lock@gmail.com>
2024-09-02 11:44:07 +0300

    Added AppHeader to AppStartpage

--- src/components/AppStartpage.jsx
+++ src/components/AppStartpage.jsx
@@ -1,7 +1,8 @@
 import PropTypes from "prop-types";
 
-import ButtonStart from "./ButtonStart";
+// import ButtonStart from "./ButtonStart";
 import AppBanner from "./AppBanner";
+import AppHeader from "./AppHeader";
 
 function AppStartpage({ onOpen }) {
   AppStartpage.propTypes = {
@@ -11,15 +12,7 @@ function AppStartpage({ onOpen }) {
   return (
     <div className="w-full h-full flex flex-col justify-center items-center p-6 sm:p-12">
       <AppBanner />
-      <div className="w-full text-right space-y-5 my-20">
-        <h1 className="font-righteous text-3xl sm:text-4xl text-sky-200 uppercase">
-          <span className="text-2xl sm:text-2xl opacity-50 block text-sky-500">
-            Welcome to
-          </span>{" "}
-          <span className="text-[#30cfd0]">Translator ReApp</span>
-        </h1>
-        <ButtonStart onOpen={onOpen} />
-      </div>
+      <AppHeader onOpen={onOpen} />
     </div>
   );
 }
