2401ad0613d3bf156e14203932b22875b7b60818
bakNa2t <ok2.lock@gmail.com>
2024-09-02 14:00:07 +0300

    Added MoonOutlined dark mode icon

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 import AppLayout from "./components/AppLayout";
 import AppStartpage from "./components/AppStartpage";
+import { MoonOutlined } from "@ant-design/icons";
 
 function App() {
   const [showApp, setShowApp] = useState(false);
@@ -15,7 +16,7 @@ function App() {
   }
 
   return (
-    <div className="w-full h-screen bg-gradient-to-l from-[#30cfd0] to-[#330867] flex justify-center items-center">
+    <div className="w-full h-screen bg-gradient-to-l from-[#30cfd0] to-[#330867] dark:bg-gradient-to-l dark:from-[#f9f586] dark:to-[#96fbc4] flex justify-center items-center">
       {/*bg-gradient-to-l from-[#7dd3fc] to-[#065f46] */}
       <div className="w-[90%] max-w-lg max-[392px]:h-[90%] sm:h-auto bg-[#27272a] rounded-xl shadow-2xl shadow-gray-800 flex flex-col">
         {showApp ? (
@@ -24,6 +25,9 @@ function App() {
           <AppStartpage onOpen={handleOpenApp} />
         )}
       </div>
+      <button>
+        <MoonOutlined className="absolute top-5 right-5 text-3xl text-gray-600" />
+      </button>
     </div>
   );
 }
