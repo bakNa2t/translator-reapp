39f642a879b2f22cfd0b1d3520ddfa7f2ca0aa5f
bakNa2t <ok2.lock@gmail.com>
2024-09-04 11:29:42 +0300

    Added Button instead ButtonStart to AppHeader and removed last one

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types";
 
-import ButtonStart from "./ButtonStart";
+import Button from "./Button";
 
 function AppHeader({ onOpen }) {
   AppHeader.propTypes = {
@@ -17,7 +17,16 @@ function AppHeader({ onOpen }) {
           Translator ReApp
         </span>
       </h1>
-      <ButtonStart onOpen={onOpen} />
+      <Button
+        className={
+          "w-40 h-10 bg-gradient-to-r from-[#30cfd0] to-[#330867] dark:bg-gradient-to-r dark:from-[#f9f586] dark:to-[#96fbc4] rounded-full font-righteous font-bold text-base uppercase text-gray-800 tracking-widest active:translate-y-[1px] hover:shadow-lg hover:shadow-sky-400 dark:hover:shadow-emerald-600 transition duration-200"
+        }
+        onClick={onOpen}
+      >
+        <span className="drop-shadow-text_2_cyan dark:drop-shadow-text_2_emerald">
+          Get Started
+        </span>
+      </Button>
     </div>
   );
 }
