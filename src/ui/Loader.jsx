95af83d704de1189e58764f16253ea04a5556cc6
bakNa2t <ok2.lock@gmail.com>
2024-09-05 16:54:39 +0300

    Updated Loader's style prop for media query

--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -1,6 +1,6 @@
 function Loader() {
   return (
-    <div className="w-12 h-12 bg-transparent rounded-full text-2xl dark:text-slate-900 text-cyan-800 flex justify-center items-center">
+    <div className="w-10 h-10 sm:w-12 sm:h-12 bg-transparent rounded-full text-2xl dark:text-slate-900 text-cyan-800 flex justify-center items-center">
       <div className="loader"></div>
     </div>
   );
