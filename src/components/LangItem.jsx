4f9b9411ce57f37b3bad99a946555d8d1565949d
bakNa2t <ok2.lock@gmail.com>
2024-09-06 10:46:01 +0300

    Updated LangItem key prop value

--- src/components/LangItem.jsx
+++ src/components/LangItem.jsx
@@ -1,17 +1,17 @@
 import PropTypes from "prop-types";
 
-function LangItem({ children, onClick, key }) {
+function LangItem({ children, onClick, langCode }) {
   LangItem.propTypes = {
     children: PropTypes.node,
     onClick: PropTypes.func,
-    key: PropTypes.string,
+    langCode: PropTypes.string,
   };
 
   return (
     <li
       className="cursor-pointer dark:hover:bg-[#a8edea] hover:bg-[#48d1cc] transition duration-200 p-2 rounded"
       onClick={onClick}
-      key={key}
+      key={langCode}
     >
       {children}
     </li>
