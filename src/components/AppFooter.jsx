f416acc10cc2e9b7b4d533aba8a2bae8d96c7f88
bakNa2t <ok2.lock@gmail.com>
2024-09-07 10:38:38 +0300

    Created AppFooter

---
+++ src/components/AppFooter.jsx
@@ -0,0 +1,19 @@
+function AppFooter() {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer className="w-full fixed bottom-1 flex justify-center items-center text-lg gap-2">
+      <span>&copy;</span> Copyright {currentYear}. Design by{" "}
+      <a
+        className="text-emerald-600 font-semibold no-underline transition-all duration-300 hover:transform hover:-translate-y-1"
+        href="https://github.com/bakna2t/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        _&#216;k
+      </a>
+    </footer>
+  );
+}
+
+export default AppFooter;
