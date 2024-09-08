7a4e59dc3e94fdac241799322a5a9bb8c51d8a4a
bakNa2t <ok2.lock@gmail.com>
2024-09-08 13:31:43 +0300

    Updated AppFooter styles

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -2,10 +2,10 @@ function AppFooter() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="fixed bottom-1 flex justify-center items-center text-[.7rem] font-light gap-2">
+    <footer className="fixed bottom-1 text-emerald-900 dark:text-slate-500 flex justify-center items-center text-[.7rem] font-light gap-2">
       <span>&copy;</span> Copyright {currentYear}. Design by{" "}
       <a
-        className="text-emerald-600 font-semibold no-underline transition-all duration-300 hover:transform hover:-translate-y-1"
+        className="text-emerald-600 dark:text-sky-400 font-semibold no-underline transition-all duration-300 hover:transform hover:-translate-y-1 hover:drop-shadow-text_2_cyan dark:hover:drop-shadow-text_2_yellow"
         href="https://github.com/bakna2t/"
         target="_blank"
         rel="noreferrer"
