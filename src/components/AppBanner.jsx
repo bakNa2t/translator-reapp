9f6f3d100cea4bbbe3ec843b3b83f2385fd3fc7b
bakNa2t <ok2.lock@gmail.com>
2024-09-09 11:01:03 +0300

    Added Typewriter to AppBanner

--- src/components/AppBanner.jsx
+++ src/components/AppBanner.jsx
@@ -1,4 +1,9 @@
 import { useEffect } from "react";
+import { Typewriter } from "react-simple-typewriter";
+
+const words = {
+  lineOne: ["Hello", "Привет", "Bonjour", "Ahoj"],
+};
 
 function AppBanner() {
   useEffect(function () {
@@ -16,7 +21,15 @@ function AppBanner() {
   return (
     <div className="w-full h-52 sm:h-64 dark:bg-gradient-to-r dark:from-[#30cfd0] dark:to-[#330867] bg-gradient-to-r from-[#f9f586] to-[#96fbc4] rounded-t-full rounded-bl-full flex flex-col justify-center dark:text-[#abecd6] text-emerald-600 pr-6 dark:shadow-sd_2 shadow-sd_4">
       <span className="font-shojumaru text-5xl sm:text-6xl text-left pl-8">
-        Hello
+        <Typewriter
+          words={words.lineOne}
+          loop={true}
+          cursor={true}
+          cursorStyle="_"
+          typeSpeed={100}
+          deleteSpeed={70}
+          delaySpeed={1500}
+        />
       </span>
       <span className="text-2xl sm:text-3xl text-center">Ciao</span>
       <span className="font-notoSansJp text-3xl sm:text-4xl text-right">
